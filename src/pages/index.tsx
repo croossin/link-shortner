7e29694aee8744645bb63d7c87c8839a32ec3e13
Chase Roossin <croossin94@gmail.com>
2023-01-29 06:43:41 -0800

    Update article url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -82,7 +82,7 @@ export default function Home() {
                 <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                   This demo is part of a blog post.{" "}
                   <a
-                    href="https://roo.app/articles"
+                    href="https://roo.app/articles/link-shortner"
                     className="font-semibold text-indigo-600"
                   >
                     <span className="absolute inset-0" aria-hidden="true" />
